7c6782032d13c97d29ef1599505ba3285b1a2f8b
Ken Shimauchi <kshimauchi@gmail.com>
2021-08-02 11:37:47 -0700

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,16 +1,25 @@
-//sample code ts to javascript
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-interface Color {
-    red: number;
-    blue: number;
-    green: number;
-}
-const color: Color = {
-    red: 10,
-    blue: 10,
-    green: 10
-};
-console.log(color);
-export default color;
 
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
+
+
+/*
+since we are extracting this from main project, need to
+install the dependencies
+
+ npm install express express-validator cookie-session jsonwebtoken @types/cookie-session @types/express @types/jsonwebtoken
+
+ 
+
+
+*/
 
