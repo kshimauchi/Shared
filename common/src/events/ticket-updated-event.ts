a725e63b77c83c723cfb6b6affc9f7aa15e195b6
Ken Shimauchi <kshimauchi@gmail.com>
2021-09-06 22:29:14 -0700

    Updates

--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -3,8 +3,9 @@ import { Subjects } from "./subjects";
 export interface TicketUpdatedEvent {
     subject: Subjects.TicketUpdated;
     data: {
-        id: string,
-        title: string,
+        id: string;
+        version: number;
+        title: string;
         price: number;
         userId: string;
     }
